2f13c089d01cb5e226573e17547a7382121fc24c
Jared Ramon Elizan <elizanjaredr@gmail.com>
2025-03-11 08:44:33 +0800

    Create Movement2.cs

---
+++ Movement2.cs
@@ -0,0 +1,25 @@
+//W A S D
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate() {
+        rb.linearVelocity = movementDirection * movementSpeed;
+    }
+}
