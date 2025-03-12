423a0e7373563f33324b1fe883f7c024db5614aa
Jared Ramon Elizan <elizanjaredr@gmail.com>
2025-03-12 12:39:08 +0800

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -1,33 +1,31 @@
-//space, A, D
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//Controls are W, A, S, D, SPACE, Arrow Keys
 
-public class Movement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
-    public float jumpStrength;
-    public float moveStrength;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 2f;
+    public float jumpForce = 7f;
+
+    public LayerMask groundLayer;
+    public Transform groundPosition;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        isGrounded = Physics2D.OverlapCircle(groundPosition.position, 0.1f, groundLayer);
+        float moveDirection = Input.GetAxisRaw("Horizontal");
+        Vector2 movement = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = movement;
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            myRigidbody.linearVelocity = Vector2.up * jumpStrength;
-        }
-        else if (Input.GetKeyDown(KeyCode.A) == true)
-        {
-            myRigidbody.linearVelocity = Vector2.left * moveStrength;
-        }
-        else if (Input.GetKeyDown(KeyCode.D) == true)
-        {
-            myRigidbody.linearVelocity = Vector2.right * moveStrength;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
